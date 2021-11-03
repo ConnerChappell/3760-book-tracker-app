792d6c8fbcf083f55b1884f9d3b6919054cd5d61
Conner Chappell <conner.chappell123@gmail.com>
2021-11-02 19:18:14 -0600

    Changed search function for better implementation with backend (hopefully)

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const searchInput = document.querySelector('.book-search-input')
 const searchBtn = document.querySelector('.book-search-btn')
 const searchResultsDiv = document.querySelector('.search-results')
+const searchingForBooks = document.querySelector('.searching-for-books-header')
+
 // let searchResults = []
+// let wantToRead = []
+// let completedBooks = []
 
 // Search input and button event listeners
 searchInput.addEventListener("keydown", (event) => {
@@ -18,37 +22,45 @@ searchBtn.addEventListener("click", (event) => {
 
 // function that fetches search results from the open library api
 function getSearchResults() {
-    searchResultsDiv.innerHTML = ""
+    searchingForBooks.innerHTML = "Searching..."
     let searchValue = searchInput.value
 
     fetch(`https://openlibrary.org/search.json?q=${searchValue}&limit=15`)
         .then((res) => res.json())
         .then((data) => {
-            data.docs.forEach((book) => {
-                addToList(
-                    book.edition_key[0],
-                    book.title,
-                    book.author_name,
-                    book.first_publish_year,
-                )
+            books = data.docs.map((book) => {
+                return {
+                    id: book.edition_key[0],
+                    key: book.key,
+                    title: book.title,
+                    author: book.author_name,
+                    yearPublished: book.first_publish_year,
+                    wishList: false,
+                    completed: false,
+                    rating: 0,
+                }
             })
+            displaySearchResults()
         })
 }
 
 // Adds cards to search results list 
-function addToList(bookKey, bookTitle, bookAuthor, yearPublished) {
-    let card = document.createElement("div")
-    card.classList.add("card")
+function displaySearchResults() {
+    searchingForBooks.innerHTML = ""
 
-    card.innerHTML = `
-        <div class="card-body" id=${bookKey}>
-            <h5 class="card-title">${bookTitle}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">by ${bookAuthor}</h6>
-            <p class="card-text">Published in ${yearPublished}</p>
-            <button type="button" class="btn btn-secondary" id="add-to-wishlist-btn">Want to Read</button>
-            <button type="button" class="btn btn-secondary" id="add-to-completed-books-btn">Completed</button>
-        </div>
-    `
+    books.forEach((book) => {
+        let card = document.createElement("div")
+        card.classList.add("card")
 
-    searchResultsDiv.appendChild(card)
+        card.innerHTML = `
+            <div class="card-body" id=${book.id}>
+                <h5 class="card-title">${book.title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">by ${book.author}</h6>
+                <p class="card-text">Published in ${book.yearPublished}</p>
+                <button type="button" class="btn btn-secondary" id="add-to-wishlist-btn">Want to Read</button>
+                <button type="button" class="btn btn-secondary" id="add-to-completed-books-btn">Completed</button>
+            </div>
+        `
+        searchResultsDiv.appendChild(card)
+    });
 }
